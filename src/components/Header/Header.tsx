4caa6d4c8abee4007cd9548b01db12008ce18f4b
Sergey <99052479+icantluvv@users.noreply.github.com>
2024-03-02 00:26:10 +0300

    update header, fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,43 @@
+import styles from './Header.module.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <header className={styles.header}>
+
+        <div className={styles.uppertext}>
+          <h1 className={styles.text}>
+              I’m open to suggestions
+          </h1>
+        </div>
+
+        <nav className={styles.navigation}>
+          <li className={styles.cont1}>
+            <a href='#ABOUT ME'>
+              <p>ABOUT ME</p>
+            </a>
+          </li>
+
+          <li className={styles.cont2}>
+            <a href='#STACK'>
+              <p>STACK</p>
+            </a>
+          </li>
+          <li className={styles.cont3}>
+            <a href='#EXPERIENCE'>
+              <p>EXPERIENCE</p>
+            </a>
+          </li>
+          <li className={styles.cont5}>
+            <a href='#PROJECTS'>
+              <p>PROJECTS</p>
+            </a>
+          </li>
+
+        </nav>
+
+      </header>
+    </div>
   )
 }
 
